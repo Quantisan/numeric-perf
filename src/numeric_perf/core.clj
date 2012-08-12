934c24f5282927ff840faa89c87a4cca085ee05c
Paul Lam <paul.lam@forward.co.uk>
2012-08-12 12:25:50 +0100

    qsort

--- src/numeric_perf/core.clj
+++ src/numeric_perf/core.clj
@@ -12,3 +12,14 @@
     (rand-int)
     (Integer/toHexString)
     (Integer/parseInt 16)))
+
+(defn qsort [[head & tail]]
+  "http://bilfp.wikidot.com/why-functional-programming-matters"
+  (when head
+    (lazy-cat (qsort (filter #(< % head) tail))
+              [head]
+              (qsort (remove #(< % head) tail)))))
+
+(defn qsort-vec [n]
+  (let [v  (take n (repeatedly rand))]
+    (doall (qsort v))))
